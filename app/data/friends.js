cea713eae0d8f72d05a4d55cbec53917a7dc414c
meeshyd <michellemdidier@gmail.com>
2017-04-30 20:43:02 -0500

    committing inital functionality

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+let friends = [
+{
+  "name":"Grumpy Cat",
+  "photoURL":"http://lorempixel.com/200/200/people/",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Stephen Colbert",
+  "photoURL":"http://lorempixel.com/200/200/people/",
+  "scores":[
+      3,
+      2,
+      5,
+      4,
+      5,
+      1,
+      1,
+      1,
+      2,
+      1
+    ]
+},
+{
+  "name":"Michelle Didier",
+  "photoURL":"http://lorempixel.com/200/200/people/",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+
+]
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,41 @@
+const friends = require("../data/friends");
+
+module.exports = function(app) {
+
+	app.get("/api/friends", function(request, response) {
+		response.json(friends);
+	});
+
+	app.post("/api/friends", function(request, response) {
+		var newFriend = request.body;
+		let differencesArray = [];
+
+		for(var x = 0; x < friends.length; x++) {
+
+			var friendToCompare = friends[x];
+			var totalDiff = 0;
+			
+			for(var y = 0; y < friendToCompare.scores.length; y++) {
+				var differenceOneScore = Math.abs(friendToCompare.scores[k] - newFriend.scores[y]);
+				totalDiff += differenceOneScore;
+			};
+
+			differencesArray[x] = totalDiff;
+		};
+
+		var bestFriendNum = differencesArray[0];
+		var bestFriendIndex = 0;
+
+		for(var i = 1; i < differencesArray.length; i++) {
+			if(differencesArray[i] < bestFriendNum) {
+				bestFriendNum = differencesArray[i];
+				bestFriendIndex = i;
+			};
+		};
+
+		friends.push(newFriend);
+
+		response.json(friends[bestFriendIndex]);
+	});
+};
+
